f6975aea05f03d5e0956d6b18b5eef296f064b39
EmilianoAprile <emilianoaprile77@icloud.com>
2023-12-12 17:25:42 +0100

    ADDED - movie cards

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -4,21 +4,37 @@
 
 <section>
     <div class="container">
-        <h1>Movies</h1>
+        <h1>Movies:</h1>
         <div class="row">
             @foreach ($movies as $movie)
 
-            <div class="col"> 
-                <h2>{{ $movie['title'] }}</h2>
-                <p>{{ $movie['original_title'] }}</p>
-                <p>{{ $movie['nationality'] }}</p>
-                <p>{{ $movie['date'] }}</p>
-                <p>{{ $movie['vote'] }}</p>
+            <div class="col-4 p-3"> 
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        <h2>{{ $movie['title'] }}</h2>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <h3>{{ $movie['original_title'] }}</h3>
+                        </li>
+                        <li class="list-group-item">
+                            <p>{{ $movie['nationality'] }}</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p>{{ $movie['date'] }}</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p>{{ $movie['vote'] }}</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
                 
             @endforeach
         </div>
     </div>
+
+    
 </section>
     
 @endsection
