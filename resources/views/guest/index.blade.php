c4ee2546051d1310ccac20084125a1987792a00c
EmilianoAprile <emilianoaprile77@icloud.com>
2023-12-12 16:26:10 +0100

    ADDED - movies data printed on page with foreach cycle

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,12 +2,27 @@
 
 @section('content')
 
-<div>
-    <h1>Index</h1>
-</div>
+<section>
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row">
+            @foreach ($movies as $movie)
+
+            <div class="col"> 
+                <h2>{{ $movie['title'] }}</h2>
+                <p>{{ $movie['original_title'] }}</p>
+                <p>{{ $movie['nationality'] }}</p>
+                <p>{{ $movie['date'] }}</p>
+                <p>{{ $movie['vote'] }}</p>
+            </div>
+                
+            @endforeach
+        </div>
+    </div>
+</section>
     
 @endsection
 
 @section('title')
-Movies
+    Movies
 @endsection
